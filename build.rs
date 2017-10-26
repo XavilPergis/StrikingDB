f2b1154b237f88dc7e242d8030a965ca2ef967cd
Ammon Smith <ammon.i.smith@gmail.com>
2017-10-25 23:23:09 -0700

    Add build.rs

---
+++ build.rs
@@ -0,0 +1,30 @@
+/*
+ * build.rs
+ *
+ * striking-db - Persistent key/value store for SSDs.
+ * Copyright (c) 2017 Maxwell Duzen, Ammon Smith
+ *
+ * striking-db is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * striking-db is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with striking-db.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+extern crate capnpc;
+
+fn main() {
+    capnpc::CompilerCommand::new()
+        .src_prefix("src")
+        .file("src/serial.capnp")
+        .run()
+        .expect("Capnp proto compilation failed");
+}
