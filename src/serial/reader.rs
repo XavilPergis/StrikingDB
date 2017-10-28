0ad3f775565b44b91cc3e7d8f2e1093a9e7a6814
Ammon Smith <ammon.i.smith@gmail.com>
2017-10-28 00:10:39 -0700

    Working on StrandReader.

--- src/serial/reader.rs
+++ src/serial/reader.rs
@@ -21,8 +21,10 @@
 
 use std::cmp;
 use std::io::{self, BufRead, Cursor, Read, Seek, SeekFrom};
+use super::error::Error;
 use super::page::Page;
 use super::strand::Strand;
+use super::utils::align;
 use super::{PAGE_SIZE, FilePointer};
 
 pub type PageReader<'a> = Cursor<&'a Page>;
@@ -30,7 +32,8 @@ pub type PageReader<'a> = Cursor<&'a Page>;
 #[derive(Debug, Clone)]
 pub struct StrandReader<'a> {
     strand: &'a Strand,
-    buffer: Page,
+    page: Page,
+    page_id: Option<u64>,
     start: usize,
     cursor: usize,
 }
@@ -39,7 +42,8 @@ impl<'a> StrandReader<'a> {
     pub fn new(strand: &'a Strand, ptr: FilePointer) -> Self {
         StrandReader {
             strand: strand,
-            buffer: Page::default(),
+            page: Page::default(),
+            page_id: None,
             start: ptr.checked_sub(strand.start()).unwrap() as usize,
             cursor: 0,
         }
@@ -47,5 +51,65 @@ impl<'a> StrandReader<'a> {
 
     pub fn into_strand(self) -> &'a Strand {
         self.strand
+    }
+
+    fn read_page(&mut self, page_id: u64) -> io::Result<()> {
+        if let Some(id) = self.page_id {
+            if id == page_id {
+                return Ok(());
+            }
+        }
+
+        let off = page_id * PAGE_SIZE;
+        self.strand.read(off, &mut self.page).map_err(Self::to_io_error)
+    }
+
+    #[inline]
+    fn get_page_id(&self) -> u64 {
+        self.cursor as u64 / PAGE_SIZE
+    }
+
+    fn to_io_error(err: Error) -> io::Error {
+        use Error::Io;
+
+        match err {
+            Io(Some(err)) => err,
+            Io(None) => io::Error::new(io::ErrorKind::Other, Io(None)),
+            _ => panic!("Non-I/O error recieved from strand I/O"),
+        }
+    }
+}
+
+impl<'a> Read for StrandReader<'a> {
+    // To avoid complex multi-page reads, only
+    // read until the end of the current page
+    //
+    // In the future we will want to optimize
+    // this for long reads that span several pages.
+    fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
+        let page_id = self.get_page_id();
+        self.read_page(page_id)?;
+        let off = self.cursor % PAGE_SIZE as usize;
+        let len = cmp::min(PAGE_SIZE as usize - off, buf.len());
+
+        let src = &self.page[off..off+len];
+        let dest = &mut buf[..len];
+        dest.copy_from_slice(src);
+
+        self.cursor += len;
+        Ok(len)
+    }
+}
+
+impl<'a> BufRead for StrandReader<'a> {
+    fn fill_buf(&mut self) -> io::Result<&[u8]> {
+        let page_id = self.get_page_id();
+        let off = self.cursor % PAGE_SIZE as usize;
+        self.read_page(page_id)?;
+        Ok(&self.page[off..])
+    }
+
+    fn consume(&mut self, amt: usize) {
+        self.cursor = cmp::min(self.cursor + amt, self.strand.capacity() as usize);
     }
 }
