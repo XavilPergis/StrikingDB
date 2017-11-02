827a754d7002adf73e9ce1ddb30aa68316289f78
Ammon Smith <ammon.i.smith@gmail.com>
2017-11-02 16:07:45 -0700

    Add getters and setters for headers.

--- src/serial/header.rs
+++ src/serial/header.rs
@@ -53,7 +53,7 @@ use self::rentals::{VolumeHeaderRental, StrandHeaderRental};
 pub struct VolumeHeader(VolumeHeaderRental);
 
 impl VolumeHeader {
-    pub fn new(strands: u16, state_ptr: u64) -> Self {
+    pub fn new(strands: u16, state_ptr: Option<u64>) -> Self {
         let message = Builder::new(PageAllocator::new());
         let fbox = unsafe { FakeBox::new(message) };
         let rental = VolumeHeaderRental::new(fbox, |message| {
@@ -61,7 +61,7 @@ impl VolumeHeader {
 
             header.set_signature(serial_capnp::VOLUME_MAGIC);
             header.set_strands(strands);
-            header.set_state_ptr(state_ptr);
+            header.set_state_ptr(state_ptr.unwrap_or(0));
 
             let (major, minor, patch) = *VERSION;
             header.set_version_major(major);
@@ -90,37 +90,80 @@ impl VolumeHeader {
         }
 
         let strands = header.get_strands();
-        let state_ptr = header.get_state_ptr();
+        let state_ptr = Self::null(header.get_state_ptr());
 
         Ok(Self::new(strands, state_ptr))
     }
-}
 
-#[derive(Debug, Hash, Clone)]
-pub struct StrandHeader {
-    id: u16,
-    capacity: u64,
-    pub offset: u64,
-    pub stats: StrandStats,
-}
+    pub fn write(self, page: &mut Page) -> Result<()> {
+        let mut slice = &mut page[..];
+        serialize_packed::write_message(&mut slice, &*self.0.into_head())?;
+        Ok(())
+    }
 
-impl StrandHeader {
-    pub fn new(id: u16, capacity: u64) -> Self {
-        StrandHeader {
-            id: id,
-            capacity: capacity,
-            offset: PAGE_SIZE64,
-            stats: StrandStats::default(),
+    fn null(ptr: u64) -> Option<u64> {
+        match ptr {
+            0 => None,
+            x => Some(x),
         }
     }
 
+    pub fn get_strands(&self) -> u16 {
+        self.0.rent(|message| message.borrow_as_reader().get_strands())
+    }
+
+    pub fn get_state_ptr(&self) -> Option<u64> {
+        self.0.rent(|message| Self::null(message.borrow_as_reader().get_state_ptr()))
+    }
+
+    pub fn set_strands(&mut self, strands: u16) {
+        self.0.rent_mut(|message| message.set_strands(strands));
+    }
+
+    pub fn set_state_ptr(&mut self, state_ptr: Option<u64>) {
+        self.0.rent_mut(|message| message.set_state_ptr(state_ptr.unwrap_or(0)));
+    }
+}
+
+pub struct StrandHeader(StrandHeaderRental);
+
+impl StrandHeader {
+    fn _new(id: u16, capacity: u64, offset: u64, stats: &StrandStats) -> Self {
+        let message = Builder::new(PageAllocator::new());
+        let fbox = unsafe { FakeBox::new(message) };
+        let rental = StrandHeaderRental::new(fbox, |message| {
+            let mut header = message.init_root::<strand_header::Builder>();
+
+            header.set_signature(serial_capnp::STRAND_MAGIC);
+            header.set_id(id);
+            header.set_capacity(capacity);
+            header.set_offset(offset);
+
+            header.set_stats_read_bytes(stats.read_bytes);
+            header.set_stats_written_bytes(stats.written_bytes);
+            header.set_stats_trimmed_bytes(stats.trimmed_bytes);
+            header.set_stats_buffer_read_bytes(stats.buffer_read_bytes);
+            header.set_stats_buffer_written_bytes(stats.buffer_written_bytes);
+            header.set_stats_valid_items(stats.valid_items);
+            header.set_stats_deleted_items(stats.deleted_items);
+
+            header
+        });
+
+        StrandHeader(rental)
+    }
+
+    pub fn new(id: u16, capacity: u64) -> Self {
+        Self::_new(id, capacity, PAGE_SIZE64, &StrandStats::default())
+    }
+
     pub fn from(strand: &Strand) -> Self {
-        StrandHeader {
-            id: strand.id(),
-            capacity: strand.capacity(),
-            offset: strand.offset(),
-            stats: strand.stats.lock().clone(),
-        }
+        Self::_new(
+            strand.id(),
+            strand.capacity(),
+            strand.offset(),
+            &strand.stats.lock().clone(),
+        )
     }
 
     pub fn read(page: &Page) -> Result<Self> {
@@ -135,6 +178,7 @@ impl StrandHeader {
         let id = header.get_id();
         let capacity = header.get_capacity();
         let offset = header.get_offset();
+
         let stats = StrandStats {
             read_bytes: header.get_stats_read_bytes(),
             written_bytes: header.get_stats_written_bytes(),
@@ -145,44 +189,64 @@ impl StrandHeader {
             deleted_items: header.get_stats_deleted_items(),
         };
 
-        Ok(StrandHeader {
-            id: id,
-            capacity: capacity,
-            offset: offset,
-            stats: stats,
-        })
+        Ok(Self::_new(id, capacity, offset, &stats))
     }
 
-    pub fn write(&self, page: &mut Page) -> Result<()> {
-        let mut message = Builder::new_default();
-        {
-            let mut header = message.init_root::<strand_header::Builder>();
-            header.set_signature(serial_capnp::STRAND_MAGIC);
-            header.set_id(self.id);
-            header.set_capacity(self.capacity);
-
-            header.set_stats_read_bytes(self.stats.read_bytes);
-            header.set_stats_written_bytes(self.stats.written_bytes);
-            header.set_stats_trimmed_bytes(self.stats.trimmed_bytes);
-            header.set_stats_buffer_read_bytes(self.stats.buffer_read_bytes);
-            header.set_stats_buffer_written_bytes(self.stats.buffer_written_bytes);
-            header.set_stats_valid_items(self.stats.valid_items);
-            header.set_stats_deleted_items(self.stats.deleted_items);
-        }
-
+    pub fn write(self, page: &mut Page) -> Result<()> {
         let mut slice = &mut page[..];
-        serialize_packed::write_message(&mut slice, &message)?;
-
+        serialize_packed::write_message(&mut slice, &*self.0.into_head())?;
         Ok(())
     }
 
-    #[inline]
-    pub fn id(&self) -> u16 {
-        self.id
+    pub fn get_id(&self) -> u16 {
+        self.0.rent(|message| message.borrow_as_reader().get_id())
     }
 
-    #[inline]
-    pub fn capacity(&self) -> u64 {
-        self.capacity
+    pub fn get_capacity(&self) -> u64 {
+        self.0.rent(|message| message.borrow_as_reader().get_capacity())
+    }
+
+    pub fn get_offset(&self) -> u64 {
+        self.0.rent(|message| message.borrow_as_reader().get_offset())
+    }
+
+    pub fn get_stats(&self) -> StrandStats {
+        self.0.rent(|message| {
+            let reader = message.borrow_as_reader();
+
+            StrandStats {
+                read_bytes: reader.get_stats_read_bytes(),
+                written_bytes: reader.get_stats_written_bytes(),
+                trimmed_bytes: reader.get_stats_trimmed_bytes(),
+                buffer_read_bytes: reader.get_stats_buffer_read_bytes(),
+                buffer_written_bytes: reader.get_stats_buffer_written_bytes(),
+                valid_items: reader.get_stats_valid_items(),
+                deleted_items: reader.get_stats_deleted_items(),
+            }
+        })
+    }
+
+    pub fn set_id(&mut self, id: u16) {
+        self.0.rent_mut(|message| message.set_id(id))
+    }
+
+    pub fn set_capacity(&mut self, capacity: u64) {
+        self.0.rent_mut(|message| message.set_capacity(capacity))
+    }
+
+    pub fn set_offset(&mut self, offset: u64) {
+        self.0.rent_mut(|message| message.set_offset(offset))
+    }
+
+    pub fn set_stats(&mut self, stats: &StrandStats) {
+        self.0.rent_mut(|message| {
+            message.set_stats_read_bytes(stats.read_bytes);
+            message.set_stats_written_bytes(stats.written_bytes);
+            message.set_stats_trimmed_bytes(stats.trimmed_bytes);
+            message.set_stats_buffer_read_bytes(stats.buffer_read_bytes);
+            message.set_stats_buffer_written_bytes(stats.buffer_written_bytes);
+            message.set_stats_valid_items(stats.valid_items);
+            message.set_stats_deleted_items(stats.deleted_items);
+        })
     }
 }
