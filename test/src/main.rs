d527443260f11c113a403249d45c8b083edd1752
Ammon Smith <ammon.i.smith@gmail.com>
2017-11-04 01:10:01 -0700

    Start operations testing.

--- test/src/main.rs
+++ test/src/main.rs
@@ -18,4 +18,26 @@ fn main() {
     };
 
     let store = Store::open(path, &options).expect("Opening datastore failed");
+
+    let mut value = [0; 16];
+
+    store.insert(b"abc", b"000").expect("1 - Insertion failed");
+    {
+        let len = store.lookup(b"abc", &mut value[..]).expect("1 - Lookup failed");
+        assert_eq!(b"000", &value[..len]);
+    }
+
+    store.update(b"abc", b"111").expect("1 - Update failed");
+    {
+        let len = store.lookup(b"abc", &mut value[..]).expect("1 - Lookup failed");
+        assert_eq!(b"111", &value[..len]);
+    }
+
+    store.insert(b"def", b"ABCDEF").expect("2 - Insertion failed");
+    {
+        let len = store.delete(b"def", &mut value[..]).expect("2 - Delete failed");
+        assert_eq!(b"ABCDEF", &value[..len]);
+
+        store.lookup(b"def", &mut value[..]).expect_err("2 - Lookup succeeded");
+    }
 }
