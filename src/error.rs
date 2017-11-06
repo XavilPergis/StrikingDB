8b6282c6d9d9beb1ca31c702a0b7e2017bdd0b42
Ammon Smith <ammon.i.smith@gmail.com>
2017-11-06 00:11:14 -0800

    Add error docs.

--- src/error.rs
+++ src/error.rs
@@ -23,21 +23,57 @@ use capnp;
 use std::fmt::{self, Display};
 use std::{error, io, result};
 
+/// A specialized [`Result`] type for this crate.
+///
+/// [`Result`]: https://doc.rust-lang.org/stable/std/io/type.Result.html
 pub type Result<T> = result::Result<T, Error>;
 
+/// The error type returned by nearly all library functions.
+/// Generally you should not match it exhaustively.
 #[derive(Debug)]
 pub enum Error {
+    /// The file used to open the datastore with is not a regular file
+    /// or block device.
     FileType,
+
+    /// Data read from the volume is corrupt. If this error occurs on open,
+    /// the file has likely not been formatted before.
     Corrupt,
+
+    /// An argument passed to a library function is invalid. A string
+    /// describing what's wrong with the argument is also returned.
     BadArgument(&'static str),
+
+    /// The given volume is valid, but its on-disk format is of an
+    /// incompatible version for this library.
     IncompatibleVersion,
+
+    /// The volume has been exhausted of free space. Generally you
+    /// should ensure the disk is 30% larger than your intended data set.
     OutOfSpace,
+
+    /// The item already exists in the datastore.
     ItemExists,
+
+    /// The item was not found in the datastore.
     ItemNotFound,
+
+    /// The key is invalid. Either it is too long, or it has a length of zero.
     InvalidKey,
+
+    /// The value is invalid. It is too long.
     InvalidValue,
+
+    /// This library function, or some aspect of it, has not been implemented yet.
     Unimplemented,
+
+    /// A network failure has occurred.
     Network,
+
+    /// An I/O error has occurred. If known, the associated [`io::Error`] is
+    /// also specified.
+    ///
+    /// [`io::Error`]: https://doc.rust-lang.org/stable/std/io/struct.Error.html
     Io(Option<io::Error>),
 }
 
