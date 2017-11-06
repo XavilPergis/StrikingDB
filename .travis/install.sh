3dc1e1c425ab0309285cd0e99cc97070c92a1b40
Ammon Smith <ammon.i.smith@gmail.com>
2017-11-06 11:44:16 -0800

    Get latest capn proto release as tarball.

--- .travis/install.sh
+++ .travis/install.sh
@@ -2,16 +2,20 @@
 set -eu
 
 case "$TRAVIS_OS_NAME" in
-    linux)
-        sudo apt-get update
-        sudo apt-get install -y capnproto
-        ;;
-    mac)
-        brew update
-        brew install capnp
-        ;;
-    *)
-        echo 'Unknown OS!'
-        exit 1
-        ;;
+	linux)
+		curl -O https://capnproto.org/capnproto-c++-0.6.1.tar.gz
+		tar zxf capnproto-c++-0.6.1.tar.gz
+		cd capnproto-c++-0.6.1
+		./configure
+		make -j6 check
+		sudo make install
+		;;
+	mac)
+		brew update
+		brew install capnp
+		;;
+	*)
+		echo 'Unknown OS!'
+		exit 1
+		;;
 esac
